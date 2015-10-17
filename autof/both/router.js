5f117b66476ca82dda27bd9cd302a6e7898e0b81
ddiazUNSJ <yddiaz@gmail.com>
2015-10-17 04:22:01 -0300

    Revert "itr begin"
    This reverts commit 13582768fb70d76b23592cce9c9809ce7641b027.

--- autof/both/router.js
+++ autof/both/router.js
@@ -5,12 +5,9 @@ Router.configure({
 });
 Router.route('/', 'posts');
 
-//Router.route('/posts/:content', function () {	
 Router.route('/posts/:_id', function () {
-   var item = Posts.findOne({_id: this.params._id});
-   this.render('post', {data: item});
- // this.render('Post'); //Cuidado si ponemos mal el nombre del template a render no hace nada
-   // this.render('post'); // Aqui tambien error nombre bien pero no tiene contexto
+  var item = Posts.findOne({_id: this.params._id});
+  this.render('post', {data: item});
 }, {
-  name: 'laposta'
+  name: 'post.show'
 });
