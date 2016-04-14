0af38cadff391f4f8b4c22a17e30b44c6fc22323
ddiazUNSJ <yddiaz@gmail.com>
2016-04-14 14:17:24 -0300

    another

---
+++ autof/client/inscri.js
@@ -0,0 +1,30 @@
+Template.InscriForm.events({
+});
+
+Template.InscriForm.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+  formCss : function() {
+    if(this.class) {
+      return this.class
+    }else {
+      return "";
+    }
+  }
+});
+
+/*****************************************************************************/
+/* Contact: Lifecycle Hooks */
+/*****************************************************************************/
+Template.InscriForm.created = function () { 
+};
+
+Template.InscriForm.rendered = function () {
+};
+
+Template.InscriForm.destroyed = function () {
+};
