3a9b8e73d49066d43e180b7905b963d6fe1e0401
ddiazUNSJ <yddiaz@gmail.com>
2016-05-24 07:03:53 -0300

    prueba de interface

---
+++ InterfaceSeed/client/config/router.js
@@ -0,0 +1,26 @@
+Router.configure({
+    layoutTemplate: 'mainLayout',
+    notFoundTemplate: 'notFound'
+
+});
+
+//
+// Example pages routes
+//
+
+Router.route('/pageOne', function () {
+    this.render('pageOne');
+});
+
+Router.route('/pageTwo', function () {
+    this.render('pageTwo');
+});
+
+Router.route('/', function () {
+    Router.go('pageOne');
+});
+
+
+Router.route('/dataTables', function () {
+    Router.go('dataTables');
+});
