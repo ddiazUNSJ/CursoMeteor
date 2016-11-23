52d90907bf071f711fb610e3af53748b94148cc5
ddiazUNSJ <yddiaz@gmail.com>
2016-11-23 07:16:12 -0300

    Arreglo portada y el archivo de configuracion setting no se almacena mas en la nube por seguridad y se debera recuperar mediantes otras vias no publicas

---
+++ 3dp/client/views/overview/schedule/schedule.js
@@ -0,0 +1,34 @@
+/*****************************************************************************/
+/* Schedule: Event Handlers and Helpers .js */
+/*****************************************************************************/
+Template.Schedule.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */ 
+});
+
+Template.Schedule.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* Schedule: Lifecycle Hooks */
+/*****************************************************************************/
+Template.Schedule.created = function () {
+};
+
+Template.Schedule.rendered = function () {
+};
+
+Template.Schedule.destroyed = function () {
+};
+
+
